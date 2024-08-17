cb0c3560a651e9a197cfd030fe5a509f48cada71
ciro <scirofranco@gmail.com>
2024-08-17 01:41:41 -0500

    Espacio de trabajo backend

---
+++ QuickParkApp/config/db.config.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const connecrDB = async () => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
